1ab43cd4a1344ab42a455e3ef2683cff10d58910
Pasesh <nastypasechnik777@mail.ru>
2026-02-10 01:17:16 +0400

    Initial commit

--- src/main/java/practice_6_hw/LinkedHashSetTasks.java
+++ src/main/java/practice_6_hw/LinkedHashSetTasks.java
@@ -14,16 +14,19 @@ public class LinkedHashSetTasks {
         System.out.println(set);
 
         //Задача 2
-        addUnique(set, "CC");
-        addUnique(set, "CA");
+        addUnique(set,"CC");
+        addUnique(set,"CA");
         System.out.println(set);
 
     }
 
-
+    //Задача 2
     public static void addUnique(LinkedHashSet<String> set, String element){
         if(!set.contains(element)){
             set.add(element);
+            System.out.println("Элемент добавлен");
+        }else {
+            System.out.println("Такой элемент уже есть");
         }
     }
 }
