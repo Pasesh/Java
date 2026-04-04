1dbdbfea13f75e7ea6325ce0512d5cc5505daace
Pasesh <101010861+Pasesh@users.noreply.github.com>
2026-04-04 22:03:03 +0400

    Delete src/main/java/practice_12 directory

--- src/main/java/practice_12/Task5/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package practice_12.Task5;
-
-public class OutOfStockException extends RuntimeException {
-    public OutOfStockException(String message) {
-        super(message);
-    }
-}
--- src/main/java/practice_12/Task6/Status.java
+++
@@ -1,9 +0,0 @@
-package practice_12.Task6;
-
-public enum Status {
-    TODO,          // задача создана, но ещё не начата
-    IN_PROGRESS,   // в работе
-    IN_REVIEW,     // на проверке (например, код-ревью)
-    BLOCKED,       // есть препятствие
-    DONE
-}
